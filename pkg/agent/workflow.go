cfcd35cc28c593387b0b7d3184354e1bbb616b03
Jeremiah Butler <git@jbutler.dev>
2025-03-21 09:02:34 -0400

    fix fail workflow on validation failure

--- pkg/agent/workflow.go
+++ pkg/agent/workflow.go
@@ -65,6 +65,7 @@ func ExecuteWorkflow(workflow []WorkflowStep, agentMap map[int]*Agent, promptInp
 
 	var err error
 	var prevResult *WorkflowResult
+	validationFailed := true
 	for i := 0; i < numValidationAttempts; i++ {
 
 		// Execute steps in sequence
@@ -94,10 +95,13 @@ func ExecuteWorkflow(workflow []WorkflowStep, agentMap map[int]*Agent, promptInp
 			ctx.Logger.Error(err, errString, "output", prevResult.Content)
 			continue
 		}
+		validationFailed = false
 		ctx.Logger.Info("Validation Succeeded")
 		break
 	}
-
+	if validationFailed {
+		return ctx.Results, fmt.Errorf("validation of workflow results failed")
+	}
 	return ctx.Results, nil
 }
 
